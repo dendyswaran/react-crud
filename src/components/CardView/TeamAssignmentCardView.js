367a0a494102c04c4e369ede4bedf19bb25f1582
numohdfauzi <numohdfauzi@deloitte.com>
2022-07-01 15:56:26 +0800

    Pagination in Assign Team

--- src/components/CardView/TeamAssignmentCardView.js
+++ src/components/CardView/TeamAssignmentCardView.js
@@ -12,7 +12,7 @@ const TeamAssignmentCardView = ({ item, href }) => {
   const equipmentStr = item.eqpCount + " Equipments";
 
   return (
-    <div className="pb-3">
+    <div className="pb-3 w-full">
       <div className="rounded-lg container bg-white w-full p-4 grid gap-4 lg:grid-cols-2 grid-cols-3">
         <div className="col-span-1">
           <ClickableHeader header="SITE ID" secondary={item.cd} />
@@ -26,11 +26,4 @@ const TeamAssignmentCardView = ({ item, href }) => {
     </div>
   );
 };
-/*
-        <div className="lg:col-span-1 inline-flex flex-row flex-wrap text-sm  col-span-2">
-          <ListingInfoCardView strInfo={equipmentStr} />
-          <ListingInfoCardView strInfo={timestampStr} />
-          <ListingInfoCardView strInfo={siteNameStr} />
-        </div>
-*/
 export default TeamAssignmentCardView;
--- src/modules/assignment/assign-team/AssignTeamPage.js
+++ src/modules/assignment/assign-team/AssignTeamPage.js
@@ -1,29 +1,19 @@
-import SiteIdSearchBar from "../../../components/SiteIdSearchBar";
 import Layout from "../../../components/Layout";
-import TeamAssignmentCardView from "../../../components/CardView/TeamAssignmentCardView";
-import PostCodeSearchBar from "../../../components/PostCodeSearchBar";
 import { Dropdown } from "primereact/dropdown";
 import InputLabel from "../../../components/FormComponents/InputLabel";
 import PrimaryButton from "../../../components/Button/PrimaryButton";
 import CheckboxMain from "../../../components/FormComponents/CheckboxMain";
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import { useLocation, useParams } from "react-router-dom";
 import { genGetDataById } from "../../../commons/GenericAction";
+import InputTextBar from "../../../components/FormComponents/InputTextBar";
+import TeamAssignmentCardView from "../../../components/CardView/TeamAssignmentCardView";
+import { DataTable } from "primereact/datatable";
+import { Ripple } from "react";
+import { Fragment } from "react";
+import { InputText } from "primereact/inputtext";
 
 export default function AssignTeamPage() {
-  //Site ID Search Query Logic
-  const { searchSite } = window.location;
-  const siteQuery = new URLSearchParams(searchSite).get("s");
-  const [searchSiteQuery, setSearchSiteQuery] = useState(siteQuery || "");
-
-  //Post Code Search Query Logic
-  const { searchPostCode } = window.location;
-  const postCodeQuery = new URLSearchParams(searchPostCode).get("pcs");
-  const [searchPostCodeQuery, setSearchPostCodeQuery] = useState(
-    postCodeQuery || ""
-  );
-
   const teamItems = [
     { name: "Team 1", value: "1" },
     { name: "Team 2", value: "2" },
@@ -40,19 +30,93 @@ export default function AssignTeamPage() {
     setSelectedTeam(e.value);
   };
 
-  //Checkbox handleChange Logic
+  //Pagination Logic
+  //Paginator and Search States
+  const [totalPages, setTotalPages] = useState(0);
+  const [pageRows, setPageRows] = useState(5);
+  const [totalElements, setTotalElements] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchSites, setSearchSites] = useState("");
+  const [pageInputTooltip] = useState("");
+
+  const recordOptions = [
+    { name: "5", value: "5" },
+    { name: "10", value: "10" },
+    { name: "20", value: "20" },
+    { name: "30", value: "30" },
+  ];
+
+  //Records Per Page Logic
+  const onRecordsChange = (e) => {
+    setPageRows(e.value);
+    //Reset current page to 1 to show first page
+    setCurrentPage(1);
+    handleData(currentPage);
+  };
+
+  //Fetch data through REST API
   const dispatch = useDispatch();
   const [sites, setSite] = useState([]);
 
-  useEffect(() => {
+  // Get data from DB
+  const handleData = (currentPage) => {
+    currentPage = currentPage - 1;
     dispatch(
-      genGetDataById(`/api/tasklist/get`, (_respData) => {
-        /** on success */
-        setSite(_respData);
-      })
+      genGetDataById(
+        `/api/tasklist/sites?page=` + currentPage + `&size=` + pageRows,
+        (_respData) => {
+          setSite(_respData.content);
+          setTotalPages(_respData.totalPages);
+          setTotalElements(_respData.totalElements);
+          setCurrentPage(_respData.number + 1);
+          console.info(`Data have been loaded successfully.`);
+        }
+      )
     );
-  }, []);
+  };
 
+  useEffect(() => {
+    handleData(currentPage);
+  }, [pageRows]);
+
+  //Search Box Method
+  const searchBox = (e) => {
+    setSearchSites(
+      //assigning value to event target
+      (e.target.name = e.target.value)
+    );
+    //handleData(currentPage);
+  };
+
+  // Search logic
+  const searchTasklist = (currentPage) => {
+    currentPage = currentPage - 1;
+    dispatch(
+      genGetDataById(
+        `/api/tasklist/sites?siteId=` +
+          searchSites +
+          `&page=` +
+          currentPage +
+          `&size=` +
+          pageRows,
+        (_respData) => {
+          setSite(_respData.content);
+          setTotalPages(_respData.totalPages);
+          setTotalElements(_respData.totalElements);
+          setCurrentPage(_respData.number + 1);
+          console.info(`Search have been loaded successfully.`);
+        }
+      )
+    );
+  };
+
+  //Reset Search Box
+  const resetSearch = () => {
+    setSearchSites("");
+    handleData(currentPage);
+  };
+
+  //Checkbox handleChange Logic
   const handleChange = (e) => {
     const { name, checked } = e.target;
 
@@ -69,71 +133,190 @@ export default function AssignTeamPage() {
     }
   };
 
-  //Site ID Filter Query Logic
-  const filterSites = (sites, siteQuery) => {
-    if (!siteQuery) {
-      return sites;
+  //Paginator States and onPageChange function
+  //Show Next page
+  const showNextPage = () => {
+    if (currentPage < Math.ceil(totalElements / pageRows)) {
+      if (!searchSites) {
+        handleData(currentPage + 1);
+      } else {
+        searchTasklist(currentPage + 1);
+      }
     }
-
-    return sites.filter((site) => {
-      const itemName = site.cd.toLowerCase();
-
-      return itemName.startsWith(siteQuery.toLowerCase());
-    });
   };
 
-  const filteredSites = filterSites(sites, searchSiteQuery);
+  //Show previous page
+  const showPrevPage = () => {
+    let prevPage = 1;
+    if (currentPage > prevPage) {
+      if (!searchSites) {
+        handleData(currentPage - prevPage);
+      } else {
+        searchTasklist(currentPage - prevPage);
+      }
+    }
+  };
+
+  const onPageInputChange = (e) => {
+    setCurrentPage(e.target.value);
+  };
+
+  const onPageInputKeyDown = (e, recordOptions) => {
+    if (e.key === "Enter") {
+      const page = parseInt(currentPage);
+      if (page < 1 || page > totalPages) {
+        console.log("Invalid Page number: ");
+        //setPageInputTooltip(`Value must be between 1 and ${totalPages}.`);
+      } else {
+        setCurrentPage(currentPage);
+        handleData(currentPage);
+      }
+    }
+  };
+
+  const template2 = {
+    layout: "RowsPerPageDropdown CurrentPageReport PrevPageLink NextPageLink",
+    PrevPageLink: (options) => {
+      return (
+        <button
+          type="button"
+          className={options.className}
+          onClick={showPrevPage}
+          disabled={currentPage === 1 ? true : false}
+        >
+          <i className="pi pi-angle-left"></i>
+        </button>
+      );
+    },
+    NextPageLink: (options) => {
+      return (
+        <button
+          type="button"
+          className={options.className}
+          onClick={showNextPage}
+          disabled={currentPage === totalPages ? true : false}
+        >
+          <i className="pi pi-angle-right"></i>
+        </button>
+      );
+    },
+    RowsPerPageDropdown: (options) => {
+      const dropdownRecordsOptions = [
+        { label: 5, value: 5 },
+        { label: 10, value: 10 },
+        { label: 20, value: 20 },
+        { label: 30, value: 30 },
+      ];
+
+      return (
+        <Fragment>
+          <span
+            className="mx-1"
+            style={{ color: "var(--text-color)", userSelect: "none" }}
+          >
+            Items per page:{" "}
+          </span>
+          <Dropdown
+            value={pageRows}
+            options={dropdownRecordsOptions}
+            onChange={onRecordsChange}
+            optionLabel="label"
+            placeholder="5"
+          />
+        </Fragment>
+      );
+    },
+    CurrentPageReport: (options) => {
+      return (
+        <span
+          className="mx-3"
+          style={{ color: "var(--text-color)", userSelect: "none" }}
+        >
+          Go to{" "}
+          <InputText
+            size="2"
+            className="ml-1"
+            value={currentPage}
+            tooltip={pageInputTooltip}
+            onKeyDown={(e) => onPageInputKeyDown(e, options)}
+            onChange={onPageInputChange}
+          />
+          <span> of {totalPages} pages</span>
+        </span>
+      );
+    },
+  };
 
   return (
     <Layout>
       <div className="pb-3">
-        <div>
-          <SiteIdSearchBar
-            searchSiteQuery={searchSiteQuery}
-            setSearchSiteQuery={setSearchSiteQuery}
+        <div className="grid grid-cols-3 gap-2">
+          <InputTextBar
+            name="search"
+            placeholder="Enter Site ID"
+            value={searchSites}
+            onChange={searchBox}
           />
-          <PostCodeSearchBar
-            searchPostCodeQuery={searchPostCodeQuery}
-            setSearchPostCodeQuery={setSearchPostCodeQuery}
+          <InputTextBar
+            name="search"
+            placeholder="Enter Postcode"
+            /* value={search}
+          onChange={searchBox} */
           />
+          <div className="col-end-7 flex justify-end">
+            <PrimaryButton name="search" onClick={searchTasklist}>
+              Search
+            </PrimaryButton>
+            <PrimaryButton name="reset" onClick={resetSearch}>
+              Reset
+            </PrimaryButton>
+          </div>
         </div>
         <div className="flex pt-8">
           <CheckboxMain
             name="allSelect"
             className="mr-2"
-            checked={!filteredSites.some((props) => props?.isChecked !== true)}
+            checked={!sites.some((props) => props?.isChecked !== true)}
             onChange={handleChange}
           />
           <label className="pt-1">Select All</label>
         </div>
         <br />
         <label>Site ID Query Results</label>
-        {filteredSites.map((props, index) => (
-          <div key={index}>
+        <br />
+        {/* Pagination here */}
+        {sites.map((properties, index) => (
+          <div key={index} className="flex in-line">
             <CheckboxMain
               className="mr-2 pt-5"
-              name={props.cd}
-              value={props.cd}
-              checked={props?.isChecked || false}
+              name={properties.cd}
+              value={properties.cd}
+              checked={properties?.isChecked || false}
               onChange={handleChange}
             />
-            <TeamAssignmentCardView key={props.key} item={props} />
+            <TeamAssignmentCardView key={properties.key} item={properties} />
           </div>
         ))}
-        <br />
-        <div className="col-span-1">
-          <InputLabel>Team</InputLabel>
-          <Dropdown
-            value={selectedTeam}
-            options={teamItems}
-            onChange={onTeamChange}
-            optionLabel="name"
-            placeholder="Select Team"
-          />
-        </div>
         <div>
-          <PrimaryButton>Assign Team</PrimaryButton>
+          <DataTable
+            value={sites}
+            paginator
+            paginatorTemplate={template2}
+          ></DataTable>
         </div>
+      </div>
+      <div className="col-span-1">
+        <InputLabel>Team</InputLabel>
+        <Dropdown
+          value={selectedTeam}
+          options={teamItems}
+          onChange={onTeamChange}
+          optionLabel="name"
+          placeholder="Select Team"
+        />
+      </div>
+      <div>
+        <PrimaryButton>Assign Team</PrimaryButton>
       </div>
     </Layout>
   );
